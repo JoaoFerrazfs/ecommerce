eda05940ef23f35f2484c0c562042efa122c76d3
JoaoFerrazfs <joaoferrazp@gmail.com>
2022-06-20 18:14:19 -0300

    feat: implements forget password

--- index.php
+++ index.php
@@ -17,19 +17,19 @@ $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 $app->get(
     '/', function (Request $request, Response $response) {
-        $page = new PageSite();
-        $page->setTpl('index');
-        return $response;
-    }
+    $page = new PageSite();
+    $page->setTpl('index');
+    return $response;
+}
 );
 
 $app->get(
     '/admin', function (Request $request, Response $response) {
-        User::verifyLogin();
-        $page = new PageAdmin();
-        $page->setTpl("index");
-        return $response;
-    }
+    User::verifyLogin();
+    $page = new PageAdmin();
+    $page->setTpl("index");
+    return $response;
+}
 );
 
 
@@ -37,138 +37,222 @@ $app->get(
     '/admin/login', function (Request $request, Response $response) {
 
 
-        $page = new PageAdmin(
-            [
+    $page = new PageAdmin(
+        [
             "header" => false,
             "footer" => false,
             "pag" => 'admin/auth/'
-            ]
-        );
-        $page->setTpl("login");
-        return $response;
-    }
+        ]
+    );
+    $page->setTpl("login");
+    return $response;
+}
 );
 
 $app->post(
     '/admin/login', function (Request $request, Response $response) {
-        User::login($_POST['login'], $_POST['password']);
-        header("Location: /admin");
-        exit;
-    }
+    User::login($_POST['login'], $_POST['password']);
+    header("Location: /admin");
+    exit;
+}
+);
+
+$app->get('/admin/forgot', function (Request $request, Response $response) {
+
+
+    $page = new PageAdmin(
+        [
+            "header" => false,
+            "footer" => false,
+            "pag" => 'admin/auth/'
+        ]
+    );
+    $page->setTpl("forgot");
+    return $response;
+}
+);
+
+$app->post('/admin/forgot', function (Request $request, Response $response) {
+    $user = User::getForgot($_POST['email']);
+
+    var_dump($user);
+    return $response
+        ->withHeader('Location', '/admin/forgot/sent')
+        ->withStatus(302);
+
+});
+
+
+$app->get('/admin/forgot/sent', function (Request $request, Response $response) {
+    $page = new PageAdmin(
+        [
+            "header" => false,
+            "footer" => false,
+            "pag" => 'admin/auth/'
+        ]
+    );
+    $page->setTpl("forgot-sent");
+
+    return $response;
+}
 );
 
 $app->get(
     '/admin/logout', function (Request $request, Response $response) {
-        User::logout();
-        header('Location: /admin/login');
-        return $response;
-    }
+    User::logout();
+    header('Location: /admin/login');
+    return $response;
+}
 );
 
 $app->get(
     '/admin/users', function (Request $request, Response $response) {
 
-        User::verifyLogin();
-        $users = User::listAll();
-        $page = new PageAdmin();
-        $page->setTpl(
-            'userManager/users', array(
+    User::verifyLogin();
+    $users = User::listAll();
+    $page = new PageAdmin();
+    $page->setTpl(
+        'userManager/users', array(
             'users' => $users
-            )
-        );
+        )
+    );
 
 
-        return $response;
+    return $response;
 
-    }
+}
 )->setName('userList');
 
 $app->get(
     '/admin/users/create', function (Request $request, Response $response) {
 
-        User::verifyLogin();
-        $page = new PageAdmin();
-        $page->setTpl('userManager/users-create');
-        return $response;
+    User::verifyLogin();
+    $page = new PageAdmin();
+    $page->setTpl('userManager/users-create');
+    return $response;
 
-    }
+}
 );
 
 $app->get(
     '/admin/users/{iduser}/delete', function (Request $request, Response $response, $idUser) {
-        User::verifyLogin();
+    User::verifyLogin();
 
-        $user = new User();
+    $user = new User();
 
-        $user->get($idUser);
+    $user->get($idUser);
 
-        $user->delete();
+    $user->delete();
 
 
-        return $response
-            ->withHeader('Location', '/admin/users')
-            ->withStatus(302);
+    return $response
+        ->withHeader('Location', '/admin/users')
+        ->withStatus(302);
 
-    }
+}
 );
 
 $app->get(
     '/admin/users/{iduser}', function (Request $request, Response $response, $iduser) {
-        User::verifyLogin();
-        $user = new User();
-        $user->get($iduser);
-        $page = new PageAdmin();
+    User::verifyLogin();
+    $user = new User();
+    $user->get($iduser);
+    $page = new PageAdmin();
 
 
-        $page->setTpl(
-            'userManager/users-update', array(
-            'user'=>$user->getValues()
-            )
-        );
-        return $response;
+    $page->setTpl(
+        'userManager/users-update', array(
+            'user' => $user->getValues()
+        )
+    );
+    return $response;
 
-    }
+}
 );
 
 $app->post(
     '/admin/users/create', function (Request $request, Response $response) use ($app) {
 
-        User::verifyLogin();
-        $user = new User();
-        $_POST['inadmin'] = (isset($_POST['inadmin'])) ? 1 : 0;
-        $user->setData($_POST);
+    User::verifyLogin();
+    $user = new User();
+    $_POST['inadmin'] = (isset($_POST['inadmin'])) ? 1 : 0;
+    $user->setData($_POST);
 
-        $user->save();
+    $user->save();
 
-        return $response
-            ->withHeader('Location', '/admin/users')
-            ->withStatus(302);
+    return $response
+        ->withHeader('Location', '/admin/users')
+        ->withStatus(302);
 
 
-    }
+}
 );
 
-$app->post(
-    '/admin/users/{iduser}', function (Request $request, Response $response, $iduser) {
-        User::verifyLogin();
+$app->post('/admin/users/{iduser}', function (Request $request, Response $response, $iduser) {
+    User::verifyLogin();
 
-        $user = new User();
+    $user = new User();
 
-        $_POST['inadmin'] = (isset($_POST['inadmin'])) ? 1 : 0;
+    $_POST['inadmin'] = (isset($_POST['inadmin'])) ? 1 : 0;
 
-        $user->get($iduser);
+    $user->get($iduser);
 
-        $user->setData($_POST);
+    $user->setData($_POST);
 
-        $user->update();
+    $user->update();
 
 
-        return $response
-            ->withHeader('Location', '/admin/users')
-            ->withStatus(302);
+    return $response
+        ->withHeader('Location', '/admin/users')
+        ->withStatus(302);
 
-    }
+}
 );
+
+$app->get('/admin/forgot/reset', function (Request $request, Response $response) {
+
+    $user = User::validForgotDecrypt($_GET['code']);
+    $page = new PageAdmin(
+        [
+            "header" => false,
+            "footer" => false,
+            "pag" => 'admin/auth/'
+        ]
+    );
+    $page->setTpl("forgot-reset", array(
+        'name' => $user['deperson'],
+        'code' => $_GET['code']
+    ));
+
+    return $response;
+
+});
+
+$app->post("/admin/forgot/reset",function (Request $request, Response $response){
+
+    $user = new User();
+    $forgot = User::validForgotDecrypt($_POST['code']);
+    User::setForgotUsed($forgot['idrecovery']);
+
+    $user->get((int)$forgot['iduser']);
+
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT,['cost'=>12]);
+
+    $user->setPassword($password);
+
+    $page = new PageAdmin(
+        [
+            "header" => false,
+            "footer" => false,
+            "pag" => 'admin/auth/'
+        ]
+    );
+    $page->setTpl("forgot-reset-success");
+    return $response;
+
+
+});
+
 
 
 $app->run();
